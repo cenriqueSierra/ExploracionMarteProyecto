601fa312bf1e8056ab710b0017a28e2a9eca7dab
domecr <djcarras@espol.edu.ec>
2022-01-09 12:46:49 -0500

    RoverEolico: Creacion clase hija

---
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/RoverEolico.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.proyectopoo2.modelo;
+
+import com.espol.proyectopoo2.interfaces.AccionesRover;
+
+/**
+ * Representa el rover de tipo eolico.
+ * @author Dome
+ */
+public class RoverEolico extends Rover { 
+    
+    /**
+     * Carga el rover en direccion donde hay mas viento.
+     */
+    @Override
+    public void cargar() {
+        
+    }
+    
+}
