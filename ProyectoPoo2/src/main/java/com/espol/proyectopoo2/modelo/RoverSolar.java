acc12302cacd590655688807bcdd3be7cdd342c4
domecr <djcarras@espol.edu.ec>
2022-01-09 12:30:13 -0500

    RoverSolar: Creacion clase hija

---
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/RoverSolar.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.proyectopoo2.modelo;
+
+/**
+ * Representa
+ * @author Dome
+ */
+public class RoverSolar extends Rover {
+    @Override
+    public void cargar() {
+        
+    }
+    
+    
+}
