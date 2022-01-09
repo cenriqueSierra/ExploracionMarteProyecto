11f341a24a7ac8c7c9216d4218244c7b0706091b
josek <jvcabrer@espol.edu.ec>
2022-01-09 13:17:15 -0500

    Crater: Creacion de parametros y metodos

--- ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/Crater.java
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/Crater.java
@@ -1,13 +1,88 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.espol.proyectopoo2.modelo;
 
 /**
  *
- * @author Carlos user
+ * @author josek
  */
 public class Crater {
+    /**
+     * Nombre del crater
+     */
+    private String nombre;
+    /**
+     * Identificador unico del crater
+     */
+    private String id;
+    /**
+     * Ubicacion del crater
+     */
+    private Ubicacion ubicacion;
+    /**
+     * Condicion de explorado o no explorado del crater
+     */
+    private boolean explorado;
+    /**
+     * Radio del crater
+     */
+    private double radio;
+
+    /**
+     * @return el nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return el id
+     */
+    public String getId() {
+        return id;
+    }
     
+    /**
+     * 
+     * @return la ubicacion del crater
+     */
+    public Ubicacion getUbicacion(){
+        return ubicacion;
+    }
+    
+    /**
+     * @return si ha sido explorado
+     */
+    public boolean isExplorado() {
+        return explorado;
+    }
+
+    /**
+     * @param explorado fija el estado de explorado
+     */
+    public void setExplorado(boolean explorado) {
+        this.explorado = explorado;
+    }
+
+    /**
+     * @return el radio
+     */
+    public double getRadio() {
+        return radio;
+    }
+    /**
+     * Constructor de la clase
+     * @param id id a fijar
+     * @param nombre nombre a fijar
+     * @param radio radio a fijar
+     * @param ubicacion ubicacion a fijar
+     */
+    public Crater(  String id,
+                    String nombre,
+                    double radio,
+                    Ubicacion ubicacion){
+        this.id = id;
+        this.nombre = nombre;
+        this.explorado = false;
+        this.radio = radio;
+        this.ubicacion = ubicacion;                
+    }
 }
