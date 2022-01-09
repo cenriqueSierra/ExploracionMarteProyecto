80e5c92ee6c6b554c51eb97a66520a87abde91bc
josek <jvcabrer@espol.edu.ec>
2022-01-09 13:29:40 -0500

    AccionesRover: Metodos default

--- ProyectoPoo2/src/main/java/com/espol/proyectopoo2/interfaces/AccionesRover.java
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/interfaces/AccionesRover.java
@@ -1,5 +1,7 @@
 package com.espol.proyectopoo2.interfaces;
 
+import com.espol.proyectopoo2.modelo.Ubicacion;
+
 /**
  * Interfaz que contiene las acciones que puede realizar el rover
  * @author josek
@@ -8,23 +10,23 @@ public interface AccionesRover {
     /**
      * Mueve al rover N pixeles adelante en la direccion que encara
      */
-    public void avanzar();
+    public default void avanzar(){};
     /**
      * Gira al rover los grados pasados como argumento en sentido de las
      * manecillas del reloj. Si el argumento es negativo, gira contrario.
      * @param grados Grados a girar el rover. En caso de ser negativo 
      * gira contrario a las manecillas del reloj.
      */
-    public void girar(double grados);
+    public default void girar(double grados){};
     /**
      * Desplaza el rover hacia la ubicacion pasada.
      * @param ubicacion Ubicacion donde se quiere mover el rover.
      */
-    //public void desplazarse(Ubicacion ubicacion);
+    public default void desplazarse(Ubicacion ubicacion){};
     /**
      * Sensa el crater por minerales.
      * @return String de minerales encontrados.
      */
-    public String sensar();     
+    public default String sensar(){return null;};     
     
 }
