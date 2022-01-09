27a81debee739b2709c28a3857817c9300801e28
domecr <djcarras@espol.edu.ec>
2022-01-09 13:26:10 -0500

    Imagenes

--- ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/Rover.java
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/Rover.java
@@ -4,22 +4,79 @@
  */
 package com.espol.proyectopoo2.modelo;
 
+import com.espol.proyectopoo2.interfaces.AccionesRover;
+
 /**
  * Clase que representa el rover
  * @author Dome user
  */
-public abstract class Rover {
+public abstract class Rover implements AccionesRover {
     /**
      * Nombre del Rover
      */
-    String nombre ;
-    //Ubicacion ubicacion;
-    String image_path;
-    //Double carga;
+    private String nombre ;
+    /**
+     * Ubicacion del rover;
+     */
+    Ubicacion ubicacion;
     
+    /**
+     * Path de la imagen del rover
+     */
+    private String image_path;
+    
+    /**
+     * Carga del rover.
+     */
+    private double carga;
+    
+   /**
+    * Crea un objeto de tipo rover
+    * @param nombre Nombre del rover
+    * @param ubicacion Ubicacion del rover
+    * @param image_path Path de la imagen del river
+    * @param carga Carga del rover
+    */ 
+   public Rover(String nombre, Ubicacion ubicacion, String image_path, double carga){
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.image_path = image_path;
+        this.carga = carga;
+        
+    }
+   
     /**
      * Carga el rover
      */
     public abstract void cargar();
+
+    /**
+     * @return  nombre de rover
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    /**
+     * @return el path de la imagen.
+     */
+    public String getImage_path() {
+        return image_path;
+    }
+
+    /**
+     * @return la carga del rover.
+     */
+    public double getCarga() {
+        return carga;
+    }
+
+    /**
+     * @param carga la carga del rover.
+     */
+    public void setCarga(double carga) {
+        this.carga = carga;
+    }
     
 }
