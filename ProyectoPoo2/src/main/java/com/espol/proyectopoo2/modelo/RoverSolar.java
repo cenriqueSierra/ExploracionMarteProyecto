5782bb0683fd0407deab21b6575416f003e67bfd
domecr <djcarras@espol.edu.ec>
2022-01-15 15:09:49 -0500

    RoverSolar:Constructor

--- ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/RoverSolar.java
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/RoverSolar.java
@@ -9,6 +9,18 @@ package com.espol.proyectopoo2.modelo;
  * @author Dome
  */
 public class RoverSolar extends Rover {
+    
+     /**
+     * Crea un rover de tipo eolico.
+     * @param nombre Nombre del rover.
+     * @param ubicacion Ubicacion del rover.
+     * @param image_path Ruta de la imagen del rover.
+     * @param carga Carga que posee el rover.
+     */
+    public RoverSolar(String nombre, Ubicacion ubicacion, String image_path, double carga){
+        super(nombre, ubicacion, image_path,carga);
+    }
+    
     /**
      * Carga el rover en la posicion donde haya mas sol.
      */
