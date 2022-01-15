a7631d89fa277667e02889c0fb45fcda5bf8018d
domecr <djcarras@espol.edu.ec>
2022-01-15 15:03:29 -0500

    Exploracion:Creacion clase

---
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/modelo/Exploracion.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.proyectopoo2.modelo;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * Clase que representa la exploracion de un crater.
+ * @author Dome
+ */
+public class Exploracion {
+    /**
+     * Fecha de la exploracion.
+     */
+    private LocalDateTime fecha;
+    /**
+     * Arreglo de minerales encontrados.
+     */
+    private ArrayList<String> minerales;
+    /**
+     * Crater explorado.
+     */
+    private Crater crater;
+    
+    
+    /**
+     * Crea un objeto exploracion.
+     * @param fecha Fecha en la que se realiza la exploracion.
+     * @param minerales Minerales encontrados.
+     * @param crater Crater explorado.
+     */
+    public Exploracion( LocalDateTime fecha, ArrayList<String> minerales,Crater crater){
+        this.fecha= fecha;
+        this.minerales = minerales;
+        this.crater = crater;
+    }
+
+    /**
+     * @return fecha de exploracion.
+     */
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @return arreglo de minerales encontrados.
+     */
+    public ArrayList<String> getMinerales() {
+        return minerales;
+    }
+
+    /**
+     * @return El crater explorado.
+     */
+    public Crater getCrater() {
+        return crater;
+    }
+    
+    
+}
