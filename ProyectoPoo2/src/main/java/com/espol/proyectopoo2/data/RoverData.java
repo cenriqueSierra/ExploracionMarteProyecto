4fd7c4a9c89ea79008134108b71095bfa5fc9d91
josek <jvcabrer@espol.edu.ec>
2022-01-15 15:33:11 -0500

    RoverData: Creacion de metodo cargarRovers y ruta del archivo

--- ProyectoPoo2/src/main/java/com/espol/proyectopoo2/data/RoverData.java
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/data/RoverData.java
@@ -4,10 +4,67 @@
  */
 package com.espol.proyectopoo2.data;
 
+import com.espol.proyectopoo2.modelo.Rover;
+import com.espol.proyectopoo2.modelo.RoverEolico;
+import com.espol.proyectopoo2.modelo.RoverSolar;
+import com.espol.proyectopoo2.modelo.Ubicacion;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
+ * Clase que contiene la informacion de los rovers
  * @author Carlos user
  */
 public class RoverData {
-    
-}
+    /**
+     * Ruta del archivo de rovers
+     */
+    public static String ruta = Constantes.ARCHIVOS+"/rovers-1.txt";
+    /**
+     * Obtiene lista de los rovers a partir del archivo
+     * @return Lista de los rovers
+     */
+    public static List<Rover> cargarRovers(){
+        List<Rover> rovers = new ArrayList<>();
+        try( BufferedReader bf =
+                new BufferedReader(new FileReader(ruta))){
+            String linea;
+            while((linea = bf.readLine())!=null){
+                System.out.println(linea);
+                String[] roverAtributos = linea.split(",");
+                String nombre =roverAtributos[0];
+                Ubicacion ubicacion = new Ubicacion(
+                        Double.parseDouble(roverAtributos[1]),
+                        Double.parseDouble(roverAtributos[2]));
+                String tipo = roverAtributos[3];
+                
+                if(tipo.equals("solar")){
+                    RoverSolar rover = new RoverSolar(
+                            nombre,
+                            ubicacion,
+                            nombre+".png",
+                            100);
+                    rovers.add(rover);
+                }else{
+                    RoverEolico rover = new RoverEolico(
+                            nombre,
+                            ubicacion,
+                            nombre+".png",
+                            100);
+                    rovers.add(rover);
+                }//else      
+            }//while
+            
+                
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        return rovers;    
+        }//metodo
+    }
+
