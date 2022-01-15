caf6643bbc5af4f826d4e230c116c0c082f352be
Carlos Sierra <cesierra@espol.edu.ec>
2022-01-15 16:56:02 -0500

    Scene arreglado

--- ProyectoPoo2/src/main/java/com/espol/proyectopoo2/App.java
+++ ProyectoPoo2/src/main/java/com/espol/proyectopoo2/App.java
@@ -24,7 +24,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         stage.setMinHeight(490);
         stage.setMaxWidth(680);
-        scene = new Scene(loadFXML("VistaInicial"), 640, 480);
+        scene = new Scene(loadFXML("VistaInicial"));
         stage.setScene(scene);
         stage.show();
     }
