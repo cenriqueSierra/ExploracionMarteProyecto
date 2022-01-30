e97c9ec76c8a5ef05f876ba0828ace7a5f49b112
Carlos Sierra <cesierra@espol.edu.ec>
2022-01-30 16:23:52 -0500

    Actualizacion de modulo para usar javafx.base

--- ProyectoPoo2/src/main/java/module-info.java
+++ ProyectoPoo2/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.espol.proyectopoo2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-
+    opens com.espol.proyectopoo2.modelo to javafx.base;
     opens com.espol.proyectopoo2 to javafx.fxml;
     exports com.espol.proyectopoo2;
 }
